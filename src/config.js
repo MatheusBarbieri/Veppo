1b746b418065e3d5735419e86dc386a0efbd98d2
MatheusBarbieri <mcb_tk@hotmail.com>
2018-12-01 23:12:32 -0200

    Fix env variable for react app

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const NODE_ENV = process.env.NODE_ENV || 'development'
-export const veppoApiHost = process.env.VEPPO_API_HOST || 'http://localhost:5000'
+export const veppoApiHost = process.env.REACT_APP_VEPPO_API_HOST || 'http://localhost:5000'
